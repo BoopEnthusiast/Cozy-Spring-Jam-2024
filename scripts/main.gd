b8c58535f41a6fac844d4ee5a2f9682f1aa582aa
Boop <onpotis@gmail.com>
2024-04-20 10:56:26 +1200

    Revert "better dragging code (it works)"
    This reverts commit dd19a169d23966f284cecfdbb53249bf72dfd67e.

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,11 +4,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _enter_tree():
 	Singleton.main_node = self
+	
 
 
-func _input(event):
-	if Input.is_action_just_pressed("click"):
-		if not Singleton.is_dragging:
-			Singleton.is_dragging = true
-	elif Input.is_action_just_released("click") and Singleton.is_dragging:
-		Singleton.is_dragging = false
