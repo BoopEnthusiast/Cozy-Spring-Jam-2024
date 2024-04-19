61bee87465cd12104603764aa0083504fa17d421
Boop <onpotis@gmail.com>
2024-04-19 15:09:00 +1200

    Fixed dragging back to what it was

--- scripts/lil_guy.gd
+++ scripts/lil_guy.gd
@@ -4,30 +4,42 @@ class_name LilGuy extends CharacterBody2D
 const SPEED = 300.0
 
 var direction: Vector2
+var mouse_is_on := false
+var is_dragging := false
 var hunger_state: int = 0
 var genes: Array[String]
-var mouse_is_on := false
 
 @onready var label = $Label
+
+
+func _input(event):
+	if Input.is_action_just_pressed("click"):
+		if mouse_is_on and not Singleton.is_dragging:
+			is_dragging = true
+			Singleton.is_dragging = true
+	elif Input.is_action_just_released("click") and is_dragging:
+		Singleton.is_dragging = false
+		is_dragging = false
 
 
 func _ready():
 	direction = Vector2(randf() - 0.5, randf() - 0.5).normalized()
 	reset_gene_display()
 
+
 func _physics_process(delta):
 	if is_on_wall():
 		direction = Vector2(randf() - 0.5, randf() - 0.5).normalized()
 	
-	if not Singleton.is_dragging:
+	if not is_dragging:
 		velocity = direction * SPEED
-	elif (Singleton.is_dragging and mouse_is_on) or hunger_state > 3:
+	elif is_dragging or hunger_state > 3:
 		velocity = Vector2.ZERO
 		global_position = get_global_mouse_position()
 	
 	move_and_slide()
 	
-	if Singleton.is_dragging and is_on_wall():
+	if is_dragging and is_on_wall():
 		var collided_object = get_last_slide_collision().get_collider()
 		if collided_object is LilGuy:
 			collided_object.genes += genes
@@ -35,12 +47,15 @@ func _physics_process(delta):
 			Singleton.is_dragging = false
 			queue_free()
 
+
 func _on_mouse_entered():
 	mouse_is_on = true
+
 
 func _on_mouse_exited():
 	if not Singleton.is_dragging:
 		mouse_is_on = false
+
 
 func _on_timer_timeout():
 	hunger_state += 1
