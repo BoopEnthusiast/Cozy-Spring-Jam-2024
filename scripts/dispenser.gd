68714a9f8040d8d7ecc7b7d125b8bf51148b6f03
Boop <onpotis@gmail.com>
2024-04-19 12:32:08 +1200

    Dispenser genes

--- scripts/dispenser.gd
+++ scripts/dispenser.gd
@@ -3,8 +3,11 @@ class_name Dispenser extends Button
 
 const LIL_GUY = preload("res://scenes/lil_guy.tscn")
 
+@export_enum("X", "Y", "Z") var gene: String
+
 
 func _on_pressed():
 	var new_lil_guy = LIL_GUY.instantiate()
 	new_lil_guy.global_position = global_position
+	new_lil_guy.genes.append(gene)
 	Singleton.main_node.add_child(new_lil_guy)
