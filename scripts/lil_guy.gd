dd19a169d23966f284cecfdbb53249bf72dfd67e
Sheep26 <crabeykiller@gmail.com>
2024-04-19 15:34:58 +1200

    better dragging code (it works)

--- scripts/lil_guy.gd
+++ scripts/lil_guy.gd
@@ -5,21 +5,10 @@ const SPEED = 300.0
 
 var direction: Vector2
 var mouse_is_on := false
-var is_dragging := false
 var hunger_state: int = 0
 var genes: Array[String]
 
 @onready var label = $Label
-
-
-func _input(event):
-	if Input.is_action_just_pressed("click"):
-		if mouse_is_on and not Singleton.is_dragging:
-			is_dragging = true
-			Singleton.is_dragging = true
-	elif Input.is_action_just_released("click") and is_dragging:
-		Singleton.is_dragging = false
-		is_dragging = false
 
 
 func _ready():
@@ -31,9 +20,9 @@ func _physics_process(delta):
 	if is_on_wall():
 		direction = Vector2(randf() - 0.5, randf() - 0.5).normalized()
 	
-	if not is_dragging:
+	if not Singleton.is_dragging:
 		velocity = direction * SPEED
-	elif is_dragging or hunger_state > 3:
+	elif (Singleton.is_dragging and mouse_is_on) or hunger_state > 3:
 		velocity = Vector2.ZERO
 		global_position = get_global_mouse_position()
 	
