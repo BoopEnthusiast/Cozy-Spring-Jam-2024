59c77adeba8440d659c4d4dd6533a78a89418c5a
Boop <onpotis@gmail.com>
2024-04-21 10:52:41 +1200

    Moving mid leave repeated code to function

--- scripts/lil_guy_sprite_display.gd
+++ scripts/lil_guy_sprite_display.gd
@@ -8,31 +8,31 @@ extends AnimatedSprite2D
 func _on_lil_guy_hunger_changed(hunger_state: int):
 	match hunger_state:
 		0:
-			play("default")
-			for mid_leaf in mid_leaves:
-				mid_leaf.play("default")
-				mid_leaf.visible = true
-			leaf_top.play("default")
-			leaf_top.visible = true
+			change_mid_leaf("default")
 		1:
-			play("hungry")
-			for mid_leaf in mid_leaves:
-				mid_leaf.play("hungry")
-				mid_leaf.visible = true
-			leaf_top.play("hungry")
-			leaf_top.visible = true
+			change_mid_leaf("hungry")
 		2:
-			play("starving")
-			for mid_leaf in mid_leaves:
-				mid_leaf.visible = false
-			leaf_top.visible = false
+			change_mid_leaf("starving")
 		3:
-			play("dead")
-			for mid_leaf in mid_leaves:
-				mid_leaf.visible = false
-			leaf_top.visible = false
+			change_mid_leaf("dead")
 
 
 func _on_lil_guy_merged():
 	#mid_leaves.reduce()
 	pass
+
+
+func change_mid_leaf(animation: String) -> void:
+	play(animation)
+	if animation == "default" or "hungry":
+		for mid_leaf in mid_leaves:
+			mid_leaf.play(animation)
+			mid_leaf.visible = true
+		leaf_top.play(animation)
+		leaf_top.visible = true
+	elif animation == "starving" or "dead":
+		for mid_leaf in mid_leaves:
+			mid_leaf.visible = false
+		leaf_top.visible = false
+	else:
+		assert(false, "Animation passed to change_mid_leaf is not a recognized animation")
