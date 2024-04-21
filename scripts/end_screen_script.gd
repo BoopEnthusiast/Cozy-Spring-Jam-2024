08680d14765ce4545d5df950d6e8e03e37a419d9
radhikadhanji <radhikadhanji0517@gmail.com>
2024-04-21 16:05:39 +1200

    fix menu screen resetting quickly

--- scripts/end_screen_script.gd
+++ scripts/end_screen_script.gd
@@ -2,7 +2,7 @@ extends Panel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if self.visible == true:
+	if Singleton.gameOver == true:
 		#wait for 30 secs then move on to picture
 		await get_tree().create_timer(30.0).timeout
 		get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
