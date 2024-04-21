62b4b8b0c7250cd47a0e8e1bd77e455f4da345a7
Sheep26 <crabeykiller@gmail.com>
2024-04-21 21:05:19 +1200

    idk man

--- scripts/feedingstation.gd
+++ scripts/feedingstation.gd
@@ -2,15 +2,10 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print(body)
 	if body is LilGuy:
 		var lilGuy: LilGuy = body
-		print("is lilGuy")
-		print(lilGuy.is_dragging)
 		if lilGuy.is_dragging:
-			print("lilGuy")
 			if Singleton.food_Left > 0:
-				print("it worky")
 				Singleton.food_Left -= 1
 				lilGuy.current_hunger_state = LilGuy.FoodLevel.HEALTHY
 				lilGuy.hunger_changed.emit()
