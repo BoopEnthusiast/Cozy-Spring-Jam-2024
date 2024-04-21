eb443b67bc97f1d4fb48cb311dae140272fe877a
Boop <onpotis@gmail.com>
2024-04-21 16:14:10 +1200

    dispensers moved

--- scripts/dispenser.gd
+++ scripts/dispenser.gd
@@ -8,6 +8,6 @@ const LIL_GUY = preload("res://scenes/lil_guy.tscn")
 
 func _on_pressed():
 	var new_lil_guy = LIL_GUY.instantiate()
-	new_lil_guy.global_position = global_position + size / 2
+	new_lil_guy.global_position = global_position + (size / 2) + Vector2(0, 500)
 	new_lil_guy.genes.append(gene)
 	Singleton.main_node.add_child(new_lil_guy)
