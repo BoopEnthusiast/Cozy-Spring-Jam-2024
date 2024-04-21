0ada9a3523fa1645134ac5f769b4c1cd77da75bf
radhikadhanji <radhikadhanji0517@gmail.com>
2024-04-21 14:49:28 +1200

    Main menu assets loaded

--- scripts/end_screen_script.gd
+++ scripts/end_screen_script.gd
@@ -1,11 +1,8 @@
 extends Panel
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if self.visible == true:
+		#wait for 30 secs then move on to picture
+		await get_tree().create_timer(30.0).timeout
+		get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
